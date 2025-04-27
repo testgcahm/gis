c781cf1027eeab005c2bf951e0d291c8f7ef1686
Mahmadabid <108066866+testgcahm@users.noreply.github.com>
2025-04-27 19:25:11 +0500

    styled a bit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,15 +8,15 @@ export default function AboutPage() {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, ease: 'easeOut' }}
-            className="flex flex-col items-center min-h-[60vh] text-secondary p-8"
+            className="flex flex-col items-center justify-center pb-[200px] min-h-[85vh] text-secondary p-8"
         >
-            <h1 className="text-2xl sm:text-4xl font-bold mb-4">About Us</h1>
+            <h1 className="text-2xl xs:text-3xl sm:text-6xl md:text-7xl font-extrabold mb-6 text-center">About Us</h1>
             <div>
                 <motion.p
                     initial={{ scale: 0.8, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     transition={{ type: 'spring', stiffness: 300, damping: 20, delay: 0.5 }}
-                    className="text-3xl xs:text-4xl sm:text-6xl font-bold text-primary-400"
+                    className="text-3xl xs:text-4xl sm:text-7xl md:text-8xl font-extrabold text-primary-400 text-center"
                 >
                     Coming Soon
                 </motion.p>
