00a4dc359145b78d30c0ea4ff7dd278714840589
testgcahm <abidahmed094@gmail.com>
2025-05-03 22:15:56 +0500

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,6 @@
 import { baseImage, baseUrl } from '@/components/utils'
 import type { MetadataRoute } from 'next'
 
-// Helper to escape XML special characters in URLs
-function escapeXml(str: string) {
-    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
-}
-
 // Helper to fetch all events
 async function fetchEvents() {
     try {
@@ -56,10 +51,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     // Fetch all events and add their dynamic URLs
     const events = await fetchEvents();
     const eventRoutes = events.map((event: any) => ({
-        url: `${baseUrl}/events/${event.slug}`,
+        url: `${baseUrl}events/${event.slug}`,
         lastModified: new Date(),
         priority: 0.76,
-        images: escapeXml(`${baseUrl}_next/image?url=%2F${event.image}&w=1080&q=75`) || baseImage
+        images: [
+            {
+                url: `${baseUrl}_next/image?url=%2F${event.image}&w=1080&q=75`
+            }
+        ]
     }));
 
     return [...staticRoutes, ...eventRoutes];
